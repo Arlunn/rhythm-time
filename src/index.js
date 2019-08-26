851326f1bf053d912f1851ff8614a0afe5d8fa74
Arlunn <32222430+Arlunn@users.noreply.github.com>
2019-08-26 19:22:38 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import Note from "./components/note";
+import Measure from "./components/measure";
+import FourMeasure from "./components/four-measures";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<FourMeasure />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

c31046cd2e25da084191ef81aef48adaa5dd6670
arlunn <ezehat@gmail.com>
2019-08-29 20:49:21 -0400

    Moved tempo functionality to measures

--- src/components/tempo.jsx
+++ src/components/tempo.jsx
@@ -1,34 +1,17 @@
 import React, { Component } from "react";
-import soundfile from "assets/beat.wav";
-import Sound from "react-sound";
 
 class Tempo extends Component {
   constructor(props) {
     super(props);
-    this.audio = new Audio(soundfile);
-    this.state = {
-      on: false
-    };
   }
-  state = {
-    on: false
-  };
   render() {
     return (
       <>
-        <div onClick={this.beginTempo()}>Play</div>
-        <audio ref={ref => (this.player = ref)} />
+        <h1 className="play" onClick={this.props.beginTempo}>
+          Play
+        </h1>
       </>
     );
-  }
-
-  beginTempo() {
-    this.setState({ on: true });
-    var intrvl = setInterval("sound()", 1000); // play beat every second
-  }
-
-  sound() {
-    this.audio.play();
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import Note from "./components/note";
 import Measure from "./components/measure";
-import FourMeasure from "./components/four-measures";
+import Measures from "./components/measures";
 
-ReactDOM.render(<FourMeasure />, document.getElementById("root"));
+ReactDOM.render(<Measures />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
